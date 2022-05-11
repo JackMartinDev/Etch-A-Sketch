90e37dbbddc7041f56b35ab1023b92c206b3d550
Jack <jackmartin.dev@gmail.com>
2022-05-11 14:50:32 +1000

    Added grid, hover over colour change

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const container = document.getElementById("container");
+container.classList.add("container");
 
 for (let i = 0; i < 16; i++) {
-  const wrapperDiv = document.createElement("div");
   for (let j = 0; j < 16; j++) {
     const div = document.createElement("div");
-    wrapperDiv.appendChild(div);
+    div.classList.add("grid");
+
+    div.addEventListener("click", () => {
+      div.style.backgroundColor = "black";
+    });
+
+    div.addEventListener("mouseover", () => {
+      div.style.backgroundColor = "blue";
+    });
+    container.appendChild(div);
   }
-  container.appendChild(wrapperDiv);
 }
 
 document.body.appendChild(container);
