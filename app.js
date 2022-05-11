051c2996c03891f361cb30939cb7bd59f301eab0
Jack <jackmartin.dev@gmail.com>
2022-05-11 14:56:34 +1000

    changed mouse click to mousemove

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ for (let i = 0; i < 16; i++) {
   for (let j = 0; j < 16; j++) {
     const div = document.createElement("div");
     div.classList.add("grid");
-
+    /*
     div.addEventListener("click", () => {
       div.style.backgroundColor = "black";
     });
-
-    div.addEventListener("mouseover", () => {
+*/
+    div.addEventListener("mousemove", () => {
       div.style.backgroundColor = "blue";
     });
     container.appendChild(div);
